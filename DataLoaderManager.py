3b88ebd101e7596b2487f925e7fd7e5acdf6cfdc
rishabhshah13 <rishabhshah13@gmail.com>
2024-04-12 19:26:48 -0400

    Added Stuff

--- DataLoaderManager.py
+++ DataLoaderManager.py
@@ -6,6 +6,8 @@ from PIL import Image
 from sklearn.model_selection import train_test_split
 from tqdm import tqdm
 
+
+
 class CustomDataset(Dataset):
     def __init__(self, data, transform=None):
         self.data = data
@@ -18,15 +20,6 @@ class CustomDataset(Dataset):
         gt_image_path = self.data[idx]['gt']
         noisy_image_path = self.data[idx]['noisy']
         
-        # print(gt_image_path)
-        # print(noisy_image_path)
-
-        # print()
-        # print()
-        # print()
-        # print()
-
-
         gt_image = Image.open(gt_image_path).convert('RGB')
         noisy_image = Image.open(noisy_image_path).convert('RGB')
         
@@ -35,7 +28,6 @@ class CustomDataset(Dataset):
             noisy_image = self.transform(noisy_image)
 
         return gt_image, noisy_image
-
 
 
 
@@ -56,16 +48,44 @@ def display_progress(cond, real, fake, figsize=(10,5)):
 
 
 
-
-    
 class DataLoaderManager:
-    def __init__(self, root_dir, transform=None):
+    def __init__(self, root_dir, train_file,test_file,make_held_out_set=False,transform=None):
         self.root_dir = root_dir
         self.transform = transform
         self.image_paths = []
-        
+        # self.train_file = f'Dataset\{train_file}_train_dataloader.npy'
+        # self.test_file = f'Dataset\{test_file}_train_dataloader.npy'
+        self.train_file = f'{train_file}_train_dataloader.npy'
+        self.test_file = f'{test_file}_train_dataloader.npy'
+        self.make_held_out_set  = make_held_out_set
+
+        folder_name = "Datasets"
+
+        # Check if the folder exists
+        if not os.path.exists(folder_name):
+            # If it doesn't exist, create it
+            os.makedirs(folder_name)
+            print(f"Folder '{folder_name}' created.")
+        else:
+            print(f"Folder '{folder_name}' already exists.")
+
+
         print("Collecting image paths...")
-        for folder_name in tqdm(os.listdir(root_dir)):
+
+        if self.make_held_out_set == True:
+            folder_list = ['0135_006_IP_00400_00400_5500_N', '0138_006_IP_00100_00100_3200_L', '0180_008_GP_00100_00100_5500_N', '0070_003_IP_02000_04000_3200_N', '0121_006_N6_03200_01000_3200_L', '0035_002_GP_00800_00350_3200_N', '0130_006_GP_00400_00400_4400_N', '0065_003_GP_10000_08460_4400_N', '0129_006_GP_00100_00100_4400_N', '0181_008_GP_00800_00800_5500_N', '0022_001_N6_00100_00060_5500_N', '0108_005_GP_06400_06400_4400_N', '0017_001_GP_00100_00060_5500_N', '0086_004_GP_00100_00100_5500_L', '0029_001_IP_00800_01000_5500_N', '0036_002_GP_06400_03200_3200_N', '0164_007_IP_00400_00400_3200_N', '0188_008_IP_00100_00100_3200_N', '0126_006_S6_00400_00200_4400_L', '0018_001_GP_00100_00160_5500_L', '0097_005_N6_03200_02000_3200_L', '0020_001_GP_00800_00350_5500_N', '0014_001_S6_03200_01250_3200_N', '0011_001_S6_00800_00500_5500_L', '0038_002_GP_00800_00640_3200_L', '0192_009_IP_00100_00200_3200_N', '0125_006_S6_00100_00050_4400_L', '0072_003_IP_01000_02000_5500_L', '0167_008_N6_00100_00050_4400_L', '0134_006_IP_00100_00100_5500_N', '0173_008_G4_00400_00400_4400_N', '0101_005_S6_00100_00050_4400_L']
+
+
+        ## linkedin Link
+        ## Primary Contact
+
+        else:
+            folder_list = os.listdir(root_dir)
+            heldout_list = ['0135_006_IP_00400_00400_5500_N', '0138_006_IP_00100_00100_3200_L', '0180_008_GP_00100_00100_5500_N', '0070_003_IP_02000_04000_3200_N', '0121_006_N6_03200_01000_3200_L', '0035_002_GP_00800_00350_3200_N', '0130_006_GP_00400_00400_4400_N', '0065_003_GP_10000_08460_4400_N', '0129_006_GP_00100_00100_4400_N', '0181_008_GP_00800_00800_5500_N', '0022_001_N6_00100_00060_5500_N', '0108_005_GP_06400_06400_4400_N', '0017_001_GP_00100_00060_5500_N', '0086_004_GP_00100_00100_5500_L', '0029_001_IP_00800_01000_5500_N', '0036_002_GP_06400_03200_3200_N', '0164_007_IP_00400_00400_3200_N', '0188_008_IP_00100_00100_3200_N', '0126_006_S6_00400_00200_4400_L', '0018_001_GP_00100_00160_5500_L', '0097_005_N6_03200_02000_3200_L', '0020_001_GP_00800_00350_5500_N', '0014_001_S6_03200_01250_3200_N', '0011_001_S6_00800_00500_5500_L', '0038_002_GP_00800_00640_3200_L', '0192_009_IP_00100_00200_3200_N', '0125_006_S6_00100_00050_4400_L', '0072_003_IP_01000_02000_5500_L', '0167_008_N6_00100_00050_4400_L', '0134_006_IP_00100_00100_5500_N', '0173_008_G4_00400_00400_4400_N', '0101_005_S6_00100_00050_4400_L']
+            new_list = [img_name for img_name in folder_list if img_name not in heldout_list]
+            folder_list = new_list
+        
+        for folder_name in tqdm(folder_list):
             folder_path = os.path.join(root_dir, folder_name)
             # print(folder_path)
             if os.path.isdir(folder_path):
@@ -74,21 +94,6 @@ class DataLoaderManager:
                         'gt': os.path.join(folder_path, image_names[0]),
                         'noisy': os.path.join(folder_path, image_names[1])
                     })
-                
-                # for image_name in os.listdir(os.path.join(folder_path)):
-                #     print(image_name)
-                #     self.image_paths.append({
-                #         'gt': os.path.join(folder_path, image_name),
-                #         'noisy': os.path.join(folder_path, image_name)
-                #     })
-            
-            # print()
-            # print()
-            # print()
-            # print()
-                
-
-        # print(self.image_paths)
 
 
     def create_datasets(self):
@@ -96,9 +101,11 @@ class DataLoaderManager:
         dataset = CustomDataset(self.image_paths, transform=self.transform)
         
         print("Splitting dataset into train and test sets...")
-        train_dataset, test_dataset = train_test_split(dataset, test_size=0.2, random_state=42)
-        
-        return train_dataset, test_dataset
+        if self.make_held_out_set == False:
+            train_dataset, test_dataset = train_test_split(dataset, test_size=0.2, random_state=42)
+            return train_dataset, test_dataset
+        else:
+            return dataset, []
 
     def save_dataloader_to_npy(self, dataloader, filename):
         print("Saving DataLoader to .npy file...")
@@ -126,17 +133,23 @@ class DataLoaderManager:
     
 
     def process_dataloaders(self, batch_size=32, shuffle=True):
-        train_filename = 'train_dataloader.npy'
-        test_filename = 'test_dataloader.npy'
-        if os.path.exists(train_filename) and os.path.exists(test_filename):
-            train_dataloader = self.load_dataloader_from_npy(train_filename, batch_size, False, is_train=True)
-            test_dataloader = self.load_dataloader_from_npy(test_filename, batch_size, False, is_train=False)
+        if os.path.exists(self.train_file) and os.path.exists(self.test_file):
+            train_dataloader = self.load_dataloader_from_npy(self.train_file, batch_size, False, is_train=True)
+            test_dataloader = self.load_dataloader_from_npy(self.test_file, batch_size, False, is_train=False)
             print("DataLoader loaded from .npy files.")
         else:
             train_dataset, test_dataset = self.create_datasets()
             train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=shuffle)
             test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
-            self.save_dataloader_to_npy(train_dataloader, train_filename)
-            self.save_dataloader_to_npy(test_dataloader, test_filename)
+            
+            
+            if self.make_held_out_set == True:
+                print(f"Creating {self.train_file}")
+                self.save_dataloader_to_npy(train_dataloader, self.train_file)
+            else:
+                print(f"Creating {self.train_file}")
+                print(f"Creating {self.test_file}")
+                self.save_dataloader_to_npy(train_dataloader, self.train_file)
+                self.save_dataloader_to_npy(test_dataloader, self.test_file)
 
         return train_dataloader, test_dataloader
